26b757854c3003b00d0dfab783b93dc99a3f4615
Rafael Okida <okida.rafael@gmail.com>
2022-06-26 03:34:49 -0300

    Projeto Galeria

--- HTML_CSS_JS/Cep/main.js
+++ HTML_CSS_JS/Cep/main.js
@@ -1,9 +1,8 @@
     function pesquisaCep(){
     
+    document.querySelector('div').style.visibility = 'visible'
     const cep = document.getElementById('cep').value
     const url = `http://viacep.com.br/ws/${cep}/json/`
-    
-
    
     //verifica se um numero foi digitado
     var validaCep = Number(cep)
@@ -14,24 +13,22 @@
         let limpaCep = document.getElementById('cep')
         limpaCep.value = ''
         document.getElementById('cep').focus()
+        
         return
      }
-    
-   
 
     fetch(url)
-    .then(response => {
-            response.json()
+    .then(response => {response.json()
        .then(dados =>{
-            inserirDados(dados)
+        inserirDados(dados)
         })
        }) 
-
     } 
  
 
     function inserirDados(data){
         
+        document.querySelector('div').style.visibility = 'hidden' 
         let logradouro = document.getElementById('logradouro')
         let bairro = document.getElementById('bairro')
         let cidade = document.getElementById('cidade')
